21eafcfe70f8259db37254deabadf5a380d1dec5
Braja1992 <brajamohanrout@outlook.com>
2025-09-10 16:33:03 +0530

    update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,21 +1,20 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://www.ageits.com'
-
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/api/',
-        '/admin/',
-        '/private/',
-        '/_next/',
-        '/search?q=',
-      ],
-    },
-    sitemap: `${baseUrl}/sitemap.xml`,
-    host: baseUrl,
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/admin/',
+          '/private/',
+          '/_next/',
+          '/search?q=',
+        ],
+      },
+    ],
+    sitemap: 'https://www.ageits.com/sitemap.xml',
   }
 }
